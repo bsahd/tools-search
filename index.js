90d5cd9ca4de1ff9481354b4fdc73711a82b6377
bsahd <193844131+bsahd@users.noreply.github.com>
2025-05-01 05:02:02 +0900

    format

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import fs from "fs"
+import fs from "fs";
 async function main() {
-	let outdata = ""
-	for (let offset = 0; true; offset+=40) {
-		const data = await (await fetch(`https://api.scratch.mit.edu/studios/27363929/projects?limit=40&offset=${offset}`)).json()
-		console.log(`${offset} items retrieved`)
+	let outdata = "";
+	for (let offset = 0; true; offset += 40) {
+		const data = await (
+			await fetch(
+				`https://api.scratch.mit.edu/studios/27363929/projects?limit=40&offset=${offset}`
+			)
+		).json();
+		console.log(`${offset} items retrieved`);
 		for (const element of data) {
-			outdata += `${element.id} ${element.username} ${element.title}\n`
+			outdata += `${element.id} ${element.username} ${element.title}\n`;
 		}
 		if (data.length < 40) {
 			break;
 		}
 	}
-	fs.writeFileSync("tools",outdata)
+	fs.writeFileSync("tools", outdata);
 }
-main()
+main();
